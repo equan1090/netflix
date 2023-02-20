a66974e3b19b022556c1ae92cc6b460ae8917e78
Eric Quan <equan1090@gmail.com>
2023-02-20 03:47:05 -0700

    changed order in init

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,6 @@
 from .db import db
 from .user import User
+from .profilefavorite import ProfileFavorite
 from .profile import Profile
 # from .show import Show
 from .favorite import Favorite
-from .profilefavorite import ProfileFavorite
