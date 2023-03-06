5fd4df06b21e1b2d6b4952a9573171624779448b
Eric Quan <equan1090@gmail.com>
2023-03-06 15:06:25 -0700

    added profile_favorites to init

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,6 @@
 from .db import db
 from .user import User
 # from .profilefavorite import ProfileFavorite
-from .profile import Profile, Favorite
+from .profile import Profile, Favorite, profile_favorite
 # from .show import Show
 # from .favorite import Favorite
